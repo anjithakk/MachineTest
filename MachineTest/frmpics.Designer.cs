8c80c97d61d93ab413c25b9dea9b1a0d12a3720d
Anjitha <anjitharaj@gmail.com>
2021-07-22 00:38:04 +0530

    Initial commit

---
+++ MachineTest/frmpics.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace MachineTest
+{
+    partial class frmPics
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlimg = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // pnlimg
+            // 
+            this.pnlimg.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pnlimg.Location = new System.Drawing.Point(206, 53);
+            this.pnlimg.Name = "pnlimg";
+            this.pnlimg.Size = new System.Drawing.Size(753, 616);
+            this.pnlimg.TabIndex = 0;
+            // 
+            // frmPics
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(1263, 744);
+            this.Controls.Add(this.pnlimg);
+            this.Name = "frmPics";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Change Pic";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlimg;
+    }
+}
+
---
+++ MachineTest/frmpics.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+
+namespace MachineTest
+{
+
+    public partial class frmPics : Form
+    {
+
+        public frmPics()
+        {
+            InitializeComponent();
+
+            System.Net.WebRequest req1 = System.Net.WebRequest.Create("https://picsum.photos/seed/picsum/200/300.jpg");
+            System.Net.WebResponse resp1 = req1.GetResponse();
+            System.IO.Stream respstream1 = resp1.GetResponseStream();
+            Bitmap bitmap1 = new Bitmap(respstream1);
+            pnlimg.BackgroundImage = bitmap1;
+
+            pnlimg.BackgroundImageLayout = ImageLayout.Stretch;
+
+            Timer tm = new Timer();
+            tm.Interval = 3000;
+            tm.Tick += new EventHandler(ChangeImages);
+            tm.Start();
+
+        }
+        private void ChangeImages(object sender, EventArgs e)
+        {
+            List<Bitmap> b = new List<Bitmap>();
+
+            System.Net.WebRequest req2 = System.Net.WebRequest.Create("https://picsum.photos/200/300/?blur.jpg");
+            System.Net.WebResponse resp2 = req2.GetResponse();
+            System.IO.Stream respstream2 = resp2.GetResponseStream();
+            Bitmap bitmap2 = new Bitmap(respstream2);
+
+            System.Net.WebRequest req3 = System.Net.WebRequest.Create("https://picsum.photos/id/237/200/300.jpg");
+            System.Net.WebResponse resp3 = req3.GetResponse();
+            System.IO.Stream respstream3 = resp3.GetResponseStream();
+            Bitmap bitmap3 = new Bitmap(respstream3);
+
+            System.Net.WebRequest req4 = System.Net.WebRequest.Create("https://picsum.photos/seed/picsum/200/300.jpg");
+            System.Net.WebResponse resp4 = req4.GetResponse();
+            System.IO.Stream respstream4 = resp4.GetResponseStream();
+            Bitmap bitmap4 = new Bitmap(respstream4);
+
+            System.Net.WebRequest req5 = System.Net.WebRequest.Create("https://picsum.photos/200/300.jpg");
+            System.Net.WebResponse resp5 = req5.GetResponse();
+            System.IO.Stream respstream5 = resp5.GetResponseStream();
+            Bitmap bitmap5 = new Bitmap(respstream5);
+
+            System.Net.WebRequest req6 = System.Net.WebRequest.Create("https://picsum.photos/200/300?grayscale.jpg");
+            System.Net.WebResponse resp6 = req6.GetResponse();
+            System.IO.Stream respstream6 = resp6.GetResponseStream();
+            Bitmap bitmap6 = new Bitmap(respstream6);
+
+
+            b.Add(bitmap2);
+            b.Add(bitmap3);
+            b.Add(bitmap4);
+            b.Add(bitmap5);
+            b.Add(bitmap6);
+
+            int index = DateTime.Now.Second % b.Count;
+            pnlimg.BackgroundImage = b[index];
+
+            pnlimg.BackgroundImageLayout = ImageLayout.Stretch;
+        }
+    }
+}
+
